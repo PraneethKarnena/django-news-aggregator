1e290c374dce6130ce08264b289e4dca693d9a59
Praneeth Karnena <praneeth.codes@gmail.com>
2019-10-01 17:23:27 +0530

    added Models

--- service/models.py
+++ service/models.py
@@ -1,3 +1,51 @@
 from django.db import models
 
-# Create your models here.
+
+class NewsSourceModel(models.Model):
+    name = models.CharField(max_length=250, null=False, blank=False)
+    code = models.CharField(max_length=3, null=False, blank=False)
+
+    top_stories_url = models.URLField(null=False, blank=False)
+    sports_url = models.URLField(null=False, blank=False)
+    entertainment_url = models.URLField(null=False, blank=False)
+    business_url = models.URLField(null=False, blank=False)
+    tech_url = models.URLField(null=False, blank=False)
+
+    def __str__(self):
+        return f'{self.name} - {self.code}'
+
+
+class UrlModel(models.Model):
+    url = models.URLField(null=False, blank=False)
+    news_source = models.ForeignKey(NewsSourceModel, on_delete=models.CASCADE, null=False, blank=False)
+
+    created = models.DateTimeField(auto_now=True)
+    updated = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-updated']
+
+    def __str__(self):
+        return f'{self.news_source.name}'
+
+
+class CategoryUrlsModel(models.Model):
+    NEWS_CAT_CHOICES = (
+        ('TPS', 'Top Stories'),
+        ('TCH', 'Technology'),
+        ('SPR', 'Sports'),
+        ('BSN', 'Business'),
+        ('ENT', 'Entertainment'),
+    )
+    news_cat = models.CharField(max_length=3, choices=NEWS_CAT_CHOICES, null=False, blank=False, \
+        unique=True)
+    urls = models.ManyToManyField(UrlModel, blank=False)
+
+    created = models.DateTimeField(auto_now=True)
+    updated = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-updated']
+
+    def __str__(self):
+        return f'{self.news_cat} - {self.news_source.name}'
