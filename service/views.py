244043e4a2204b97ddd5ba44b5c09a603ec0d352
Praneeth Karnena <praneeth.codes@gmail.com>
2019-10-01 16:43:57 +0530

    implemented Home page

--- service/views.py
+++ service/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.views.decorators.http import require_http_methods
 
-# Create your views here.
+
+@require_http_methods(['GET'])
+def home_view(request):
+    return render(request, 'service/home.html')
