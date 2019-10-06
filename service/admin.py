abb8c5f39ce81f3586712ee2d8f3d4baaeb78377
Praneeth Karnena <praneeth.codes@gmail.com>
2019-10-06 22:14:28 +0530

    added admin functionality for all models

--- service/admin.py
+++ service/admin.py
@@ -2,3 +2,5 @@ from django.contrib import admin
 from service import models
 
 admin.site.register(models.NewsSourceModel)
+admin.site.register(models.UrlModel)
+admin.site.register(models.CategoryUrlsModel)
